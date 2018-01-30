65f3d4ca7b9b66d689cf323424519c8bb281c06b
darknesswind <darknesswind@qq.com>
2018-01-30 23:52:50 +0800

    on develop

--- bakalng/scanner/scanner.cpp
+++ bakalng/scanner/scanner.cpp
@@ -1,6 +1,6 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "scanner.h"
-#include "LTextStream.h"
+
 
 Scanner::Scanner()
 {
@@ -13,20 +13,65 @@ Scanner::~Scanner()
 
 void Scanner::scan(const char* szFile)
 {
-	LTextStream stream;
-	stream.openRead(szFile);
-	procFileScope(stream);
+	m_stream.openRead(szFile);
+
+	const FuncProcState proc[stMax] =
+	{
+		&Scanner::procFileScope,
+		&Scanner::procStringScope
+	};
+
+	enterState(stNormal);
+	
+	while (!m_stream.eof())
+	{
+		char16_t ch = m_stream.readChar();
+		(this->*proc[m_stkState.back().type])(ch);
+		++m_char;
+	}
 }
 
-void Scanner::procFileScope(LTextStream& stream)
+void Scanner::enterState(StateType st)
 {
-	enum State
-	{
-		stNormal,
-	};
-	while (!stream.isEnd())
-	{
-		char16_t ch = stream.readChar();
+	m_stkState.push_back(State{ st, m_line, m_char });
+}
 
+void Scanner::leaveState(StateType st)
+{
+	const State& state = m_stkState.back();
+	assert(state.type == st);
+	switch (st)
+	{
+	case Scanner::stString:
+
+		break;
+	default:
+		break;
 	}
+	m_stkState.pop_back();
+}
+
+void Scanner::procFileScope(char16_t ch)
+{
+	switch (ch)
+	{
+	case u'"':
+		enterState(stString);
+		return;
+	default:
+		break;
+	}
+}
+
+void Scanner::procStringScope(char16_t ch)
+{
+	if (ch == u'"')
+		leaveState(stString);
+	else
+		m_buf.push_back(ch);
+}
+
+void Scanner::genStringToken(const State& st)
+{
+
 }
--- bakalng/scanner/scanner.h
+++ bakalng/scanner/scanner.h
@@ -1,8 +1,22 @@
-#pragma once
+﻿#pragma once
+#include "LTextStream.h"
+#include "LString.h"
+#include "token.h"
 
-class LTextStream;
 class Scanner
 {
+	enum StateType
+	{
+		stNormal,
+		stString,
+		stMax,
+	};
+	struct State
+	{
+		StateType type;
+		size_t beginLine;
+		size_t beginChar;
+	};
 public:
 	Scanner();
 	~Scanner();
@@ -10,6 +24,24 @@ public:
 	void scan(const char* szFile);
 
 private:
-	void procFileScope(LTextStream& stream);
+	void enterState(StateType st);
+	void leaveState(StateType st);
+
+	typedef void (Scanner::*FuncProcState)(char16_t);
+	void procFileScope(char16_t ch);
+	void procStringScope(char16_t ch);
+
+private:
+	void genStringToken(const State& st);
+	
+
+private:
+	LTextStream m_stream;
+	std::vector<State> m_stkState;
+	std::vector<Token> m_tokens;
+
+	LString m_buf;
+	size_t m_line{ 0 };
+	size_t m_char{ 0 };
 };
 
